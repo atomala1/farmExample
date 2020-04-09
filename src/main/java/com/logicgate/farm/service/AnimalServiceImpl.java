3754ab680e27cd119beb55bd4adf024a9733e3d2
Alex Tomala <atomala1@gmail.com>
2020-04-09 10:36:15 -0500

    Adding more comments.

--- src/main/java/com/logicgate/farm/service/AnimalServiceImpl.java
+++ src/main/java/com/logicgate/farm/service/AnimalServiceImpl.java
@@ -16,6 +16,30 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.*;
 import java.util.stream.Collectors;
 
+/**
+ * I chose this implementation since the data set is small and the Animal/Barn objects are small.  I assumed
+ * it would be fine to slice the animals/barns by color and pull them all in with the same color.  I also assumed
+ * it would be fine to pull in all animals/barns since this is an in memory H2 database and there is no network
+ * overhead pulling in the entire set of animals with the same color.
+ * I minimized the amount of calls to update animals during rebasing by only moving the animals that need to be moved
+ * and to issue a saveAll call instead of updating them one by one.
+ *
+ * I thought of a few other implementations that would optimize for different cases, but chose not to implement them
+ * at this point.
+ *
+ * If we wanted to minimize back and forth between the database, I could have done a mathematical approach as follows:
+ * 1. Get the total number of animals by color: (select count(1) from animal where favorite_color = :color)
+ * 2. Get a map of animals to barn id: (select barn_id, count(1) from animal where favorite_color = :color group by barn_id)
+ * 3. Do some math to make sure adding an animal to the least populated barn won't make it go above the max capacity.
+ * 4. If rebalancing is needed, you can figure out the number you need to move and just issue the update statements by hand.
+ * 5. When removing a barn, you can just redistribute the animals the same way.
+ *
+ * I also thought about using the barn's name field as a sort of metadata field since it isn't being used for anything.
+ * I could store the animal IDs in a barn in a comma separated list and then move most of the heavy lifting to a
+ * BarnService instead.  I didn't choose this approach because then things become more brittle since data is stored
+ * in two places instead of just once.
+ *
+ */
 @Service
 @Transactional
 @AllArgsConstructor
@@ -46,7 +70,8 @@ public class AnimalServiceImpl implements AnimalService {
     Map<Barn, List<Animal>> barnToAnimalsMap = getBarnToAnimalsMap(animal.getFavoriteColor());
     Optional<Map.Entry<Barn, List<Animal>>> leastPopulatedBarn = getLeastPopulatedElement(barnToAnimalsMap);
 
-    if (!leastPopulatedBarn.isPresent() || leastPopulatedBarn.get().getValue().size() >= leastPopulatedBarn.get().getKey().getCapacity()) {
+    // If there is no least populated barn, or if the least populated barn is at capacity, we need to create a new barn
+    if (!leastPopulatedBarn.isPresent() ||leastPopulatedBarn.get().getValue().size() >= leastPopulatedBarn.get().getKey().getCapacity()) {
       // A new barn needs to be created
       Barn barn = barnRepository.save(new Barn(FarmUtils.barnName(barnToAnimalsMap.size()), animal.getFavoriteColor()));
 
@@ -76,11 +101,11 @@ public class AnimalServiceImpl implements AnimalService {
     Preconditions.checkNotNull(animal.getId(), "The animal must have an ID.");
     Preconditions.checkNotNull(animal.getBarn(), "The animal must be in a barn.");
 
-    // Make sure the user passed us an existing animal.
+    // Make sure the user passed us an animal that exists.
     Animal foundAnimal = animalRepository.findById(animal.getId())
         .orElseThrow(() -> new AnimalNotFoundException(String.format("Animal with ID %s not found.", animal.getId())));
 
-    // Just delete the animal first.
+    // Delete the animal first.
     animalRepository.delete(foundAnimal);
 
     Map<Barn, List<Animal>> barnToAnimalsMap = getBarnToAnimalsMap(foundAnimal.getFavoriteColor());
@@ -96,7 +121,7 @@ public class AnimalServiceImpl implements AnimalService {
       // Delete a barn and rebalance
       removeBarnAndRebalanceAnimals(barnToAnimalsMap, animal.getBarn());
     } else {
-      // Need to find if we need to move some animals around after the removal
+      // Find if we need to move some animals around after the removal
       rebalanceAnimals(barnToAnimalsMap);
     }
   }
@@ -106,6 +131,9 @@ public class AnimalServiceImpl implements AnimalService {
     animals.forEach(animal -> removeFromFarm(animalRepository.getOne(animal.getId())));
   }
 
+  /**
+   * Return a map of barn to the list of animals in the barn.
+   */
   private Map<Barn, List<Animal>> getBarnToAnimalsMap(Color color) {
     List<Animal> animals = animalRepository.findByFavoriteColor(color);
 
@@ -119,6 +147,12 @@ public class AnimalServiceImpl implements AnimalService {
     return barnToAnimalsMap;
   }
 
+  /**
+   * Rebalance animals based on unused capacity.  If the difference between the unused capacity is greater than
+   * one, we need to move some animals from the most populated barn to the least populated barn.
+   *
+   * When we rebalance, there should always be an entry in the map.  The exception is to protect against future updates.
+   */
   private void rebalanceAnimals(Map<Barn, List<Animal>> barnToAnimalsMap) {
     List<Animal> updatedAnimals = new ArrayList<>();
     List<Integer> unusedCapacity = getUnusedCapacity(barnToAnimalsMap);
@@ -142,9 +176,13 @@ public class AnimalServiceImpl implements AnimalService {
       unusedCapacity = getUnusedCapacity(barnToAnimalsMap);
     }
 
+    // Issue an update at the end to minimize DB calls.
     animalRepository.saveAll(updatedAnimals);
   }
 
+  /**
+   * Get a list of open spaces in all the barns.
+   */
   private List<Integer> getUnusedCapacity(Map<Barn, List<Animal>> barnToAnimalsMap) {
     return barnToAnimalsMap.keySet()
         .stream()
@@ -152,6 +190,11 @@ public class AnimalServiceImpl implements AnimalService {
         .collect(Collectors.toList());
   }
 
+  /**
+   * If we need to delete a barn, we should redistribute the animals in that barn to the other barns.
+   * Animals must be redistributed before the barn is deleted, otherwise the foreign key constraint will stop us
+   * from deleting the barn.
+   */
   private void removeBarnAndRebalanceAnimals(Map<Barn, List<Animal>> barnToAnimalMap, Barn barn) {
     if (!barnToAnimalMap.isEmpty()) {
       Map.Entry<Barn, List<Animal>> entryToRemove =
